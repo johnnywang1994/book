1355787d1c2e68820f7893c4a32dc954b671de15
Lindy Liao <meiliao1207@gmail.com>
2025-05-13 14:37:17 +0800

    UPD: update cdn base

--- docs/.vuepress/components/Spine/FinalGear.vue
+++ docs/.vuepress/components/Spine/FinalGear.vue
@@ -43,11 +43,11 @@ const router = useRouter()
 const route = useRoute()
 
 const BASE_URL = process.env.NODE_ENV === 'production'
-  ? 'https://cdn.statically.io/gh/johnnywang1994/spine-assets/master/gear'
-  : 'https://cdn.statically.io/gh/johnnywang1994/spine-assets/master/gear'
+  ? 'https://cdn.statically.io/gh/jwlearn1994/spine-assets/master/gear'
+  : 'https://cdn.statically.io/gh/jwlearn1994/spine-assets/master/gear'
   // : '/book/Resources/spine/gear';
 
-const headImg = (id) => `https://cdn.statically.io/gh/johnnywang1994/spine-assets/master/gear/head/${id}.png`;
+const headImg = (id) => `https://cdn.statically.io/gh/jwlearn1994/spine-assets/master/gear/head/${id}.png`;
 
 const state = reactive({
   open: false,
